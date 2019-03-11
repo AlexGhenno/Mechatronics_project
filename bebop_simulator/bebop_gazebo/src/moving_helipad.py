3caa5f7f7ef27d6bc7becb4c24457a9373e39039
AlexGhenno <ghennoba@gmail.com>
2019-03-11 13:03:24 +0100

    Add files via upload

--- bebop_simulator/bebop_gazebo/src/moving_helipad.py
+++ bebop_simulator/bebop_gazebo/src/moving_helipad.py
@@ -10,10 +10,22 @@ x_vel = 1.0
 y_vel = 0.5
 cnt = 0
 r = 1 
-t =1
+
+t = 0
+px = 3
+py = 0
+
+#trajectory dimensions
+path_length = 5
+path_width = 2
+t_step = 0.05
+path_resolution = 0.001
+
+
 model_name = ""
 model_state_pub = None
 model_state_sub = None
+#rate = rospy.Rate(1) # 10h
 
 def init():
     global model_name, model_state_pub, x_vel, y_vel, model_state_sub
@@ -29,33 +41,56 @@ def init():
         rospy.spin()
 
 def modelStatesCallback(msg):
-    global model_name, model_state_pub, x_vel, y_vel, model_state_sub, r, t
+    global model_name, model_state_pub, x_vel, y_vel, model_state_sub, r, t, rate
     index_of_interest = -1			#index of the last element in the model_states msg provided by gazebo, which corresponds to the pose information of the helipad.
-    t=0;
     for i in range(len(msg.name)):
         if msg.name[i] == model_name:		#way to ensure that the index actually corresponds to the helipad's index
             index_of_interest = i
             break
-    
-    if t >=310:
-    	t =0
+    '''
+    if t >=310.0:
+    	t =0.0
     else:
-	t=i+1
-        vx=math.cos(0.2*t)
-        vy=math.sin(0.2*t)
+	    t=i+1
+        px=math.cos(0.2*t)
+        py=math.sin(0.2*t)
+        print(px)
+    '''  
+    #rate.sleep()
+    now = rospy.get_rostime()
+    if (now.nsecs % 1000000) == 0:
+        if t>6300:
+            t = 0
+        else:
+            t = t + t_step
+        
+    px = path_length*(math.cos(path_resolution*t))
+    py = path_width*(math.sin(2*path_resolution*t))   
 
     if index_of_interest >= 0:
         model_state = ModelState()
         model_state.model_name = model_name
-        model_state.pose = msg.pose[index_of_interest]
-        twist = Twist()
-        #pose.position.x = vx
-        #pose.position.y =vy
-	twist.linear.x=x_vel
-	twist.linear.y=y_vel
-        if msg.twist[index_of_interest] != twist:
-            model_state.twist = twist
+        #model_state.pose = msg.pose[index_of_interest]
+        model_state.twist = msg.twist[index_of_interest]
+        #twist = Twist()
+        #px = 2
+        #py = px
+        pose = Pose()
+        pose.position.x = px
+        pose.position.y =py
+
+	    #twist.linear.x=x_vel
+	    #twist.linear.y=y_vel
+
+        #if msg.twist[index_of_interest] != twist:
+        if msg.pose[index_of_interest] != pose:
+            #model_state.twist = twist
+            model_state.pose = pose
             model_state_pub.publish(model_state)
+            #print(px)
+            #print(py)
+            #print(t)
+            #print(now)
 
 if __name__ == '__main__':
     init()
